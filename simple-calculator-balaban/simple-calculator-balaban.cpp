4ef883bec2d5a5a64854ba3184d618b550639077
balaban <vova.balaban.341@gmail.com>
2026-04-17 23:13:12 +0300

    add start logging

--- simple-calculator-balaban/simple-calculator-balaban.cpp
+++ simple-calculator-balaban/simple-calculator-balaban.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::cout << "Simple calculator" << std::endl;
+    std::cout << "Calculator started" << std::endl;
 
     double a = getNumber("Enter first number: ");
     double b = getNumber("Enter second number: ");
