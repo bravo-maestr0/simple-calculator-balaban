5ad118e68235d02d7c798d29a3ba26c3fd4bf6dc
balaban <vova.balaban.341@gmail.com>
2026-04-19 16:15:58 +0300

    prepare code for review

--- simple-calculator-balaban/simple-calculator-balaban.cpp
+++ simple-calculator-balaban/simple-calculator-balaban.cpp
@@ -4,7 +4,9 @@
 
 int main() {
     std::cout << "Simple calculator" << std::endl;
-    std::cout << "Calculator started" << std::endl;
+    std::cout << "Calculator started (review version)" << std::endl;
+
+    //std::cout << "Calculator started" << std::endl;
 
     double a = getNumber("Enter first number: ");
     double b = getNumber("Enter second number: ");
